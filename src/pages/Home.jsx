37767982370eda27b3c2b0004d0fec331800cfb4
shameCanales <shamecanales1082@gmail.com>
2025-07-12 14:28:52 +0800

    added fetching products feature for products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchProducts } from "../util/http";
-import { useEffect } from "react";
 
 export default function Home() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["products", { max: 20 }],
+    queryKey: ["products"],
     queryFn: ({ signal }) => fetchProducts({ signal }),
     staleTime: 2 * 60 * 1000,
   });
 
-  useEffect(() => {
-    if (data) {
-      console.log("fetched products:", data);
-    }
-  }, [data]);
-
   let content = <p>Refresh to see products</p>;
+
+  if (isPending) {
+    content = <p>Getting available products for you...</p>;
+  }
+
+  if (isError) {
+    content = <p>Something went wrong: {error.message}</p>;
+  }
 
   if (data) {
     content = (
       <ul>
         {data.map((product) => (
-          <li key={product.id}>{product.title}</li>
+          <li key={product.id}>
+            <p>{product.title}</p>
+            {product.images?.[0] && (
+              <img src={product.images[0]} alt={product.title} />
+            )}
+            <p>price: {product.price}</p>
+            <p>description: {product.description}</p>
+          </li>
         ))}
       </ul>
     );
   }
-
   return (
     <>
       <h1>FakeStore Items:</h1>
